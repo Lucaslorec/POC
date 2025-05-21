6ad89a37d1bff8aa2218281e49928b44630a0180
Lucas <lucas.ballyn@otamiser.com>
2025-05-21 16:03:39 +0200

    feat: Add rebelInternetClient with methods for tracking and retrieving ranking results

---
+++ src/poc/ranking-api/clients/rebel-internet.ts
@@ -0,0 +1,58 @@
+import axios from 'axios';
+import { config } from '../config';
+
+
+
+class rebelInternetClient {
+    private baseUrl: string;
+    private headers: Record<string, string>;
+
+    constructor(baseUrl: string, apiKey: string) {
+        this.baseUrl = baseUrl;
+        this.headers = {
+            'x-api-key': apiKey,
+            'Content-Type': 'application/json'
+        }
+    }
+
+    factory() {
+        const baseUrl = config.dataSourceUrl
+        const apiKey = config.apiKey
+        return new rebelInternetClient(baseUrl, apiKey);
+    }
+
+    track(params: {
+        propertyUid: string;
+        dateType: string;
+        domain: string;
+        location: {
+            neLat: number | null;
+            neLng: number | null;
+            swLat: number | null;
+            swLng: number | null;
+            zoomLevel: number | null;
+            query: string;
+        };
+        adults: number;
+    }) {
+        return axios.post(`${this.baseUrl}/query`, params, {
+            headers: this.headers
+        });
+    }
+
+    getRankingResults(params: {
+        id: number;
+        from: string;
+        until: string;
+    }) {
+        return axios.get(`${this.baseUrl}/query/${params.id}`, {
+            params: {
+                from: params.from,
+                until: params.until,
+            },
+            headers: this.headers,
+        });
+    }
+}
+
+export default rebelInternetClient;
